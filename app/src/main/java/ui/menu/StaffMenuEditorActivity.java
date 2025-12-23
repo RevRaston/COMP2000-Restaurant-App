081a06f01ca742f95ef86fc5e7f4dd9edc769ed2
RevRaston <ryan.aston@students.plymouth.ac.uk>
2025-12-23 22:42:52 +0000

    stage 3.3: staff menu editor stub and menu UX improvements

---
+++ app/src/main/java/ui/menu/StaffMenuEditorActivity.java
@@ -0,0 +1,15 @@
+package com.example.comp2000restaurantapp.ui.menu;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.comp2000restaurantapp.R;
+
+public class StaffMenuEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_menu_editor);
+    }
+}
+
